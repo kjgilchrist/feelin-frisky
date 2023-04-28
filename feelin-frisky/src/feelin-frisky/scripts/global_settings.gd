c91dba8d402471d08f0714bf2d8ef933d22df19d
Katherine Gilchrist <93557317+kjgilchrist@users.noreply.github.com>
2023-04-28 14:21:36 -0600

    spawn blurry items

--- feelin-frisky/src/feelin-frisky/scripts/global_settings.gd
+++ feelin-frisky/src/feelin-frisky/scripts/global_settings.gd
@@ -2,15 +2,18 @@ extends Node
 
 signal fps_displayed(value)
 signal brightness_updated(value)
+# Will load Item textures into global array.
+var items_deadly_paths = []
+var items_safe_paths = []
 # Best Stats
 var max_frisks
 var max_items
 var min_casualties
 var best_job_status
 
-
 func _ready():
-	pass
+	dir_contents("res://src/feelin-frisky/art/items_deadly", items_deadly_paths)
+	dir_contents("res://src/feelin-frisky/art/items_safe", items_safe_paths)
 
 
 func toggle_fullscreen(value):
@@ -43,3 +46,21 @@ func update_brightness(value):
 func update_bus_vol(bus_idx, value):
 	# Master Bus - 0, Music - 1, SFX - 2
 	AudioServer.set_bus_volume_db(bus_idx, value)
+
+
+func dir_contents(path, array):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+				if file_name.get_extension() == "png":
+					array.append(file_name)
+					#print(file_name + " added to array " + str(array))
+				file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
--- feelin-frisky/src/feelin-frisky/scripts/item.gd
+++ feelin-frisky/src/feelin-frisky/scripts/item.gd
@@ -1,21 +1,30 @@
 extends Sprite2D
 
-@onready var safe_texture = preload("res://src/feelin-frisky/art/kenney_prototype_textures/green/texture_01.png")
-@onready var not_safe_texture = preload("res://src/feelin-frisky/art/kenney_prototype_textures/red/texture_01.png")
+#@onready var safe_texture = preload("res://src/feelin-frisky/art/kenney_prototype_textures/green/texture_01.png")
+#@onready var not_safe_texture = preload("res://src/feelin-frisky/art/kenney_prototype_textures/red/texture_01.png")
+@onready var safe_texture = preload("res://src/feelin-frisky/art/items_safe/bottle.png")
+@onready var not_safe_texture = preload("res://src/feelin-frisky/art/items_deadly/gun.png")
 var is_deadly : bool
 
-
 func _enter_tree():
+	randomize()
 	# Generate random choice in array here.
-	is_deadly = randi() % 2 == 0
-	print(is_deadly)
+	is_deadly = randi() % 3 == 0
+	#print(is_deadly)
+	rotation = randi_range(-1,1)
 
 
 func _ready():
 	# This is just for debugging purposes - change to random within array!
 	if is_deadly:
-		set_texture(not_safe_texture)
+		var rand_png = GlobalSettings.items_deadly_paths[randi() % GlobalSettings.items_deadly_paths.size()]
+		var rand_texture = load("res://src/feelin-frisky/art/items_deadly/" + rand_png)
+		set_texture(rand_texture)
 	else:
-		set_texture(safe_texture)
-	scale.x = 0.025
-	scale.y = 0.025
+		var rand_png = GlobalSettings.items_safe_paths[randi() % GlobalSettings.items_safe_paths.size()]
+		var rand_texture = load("res://src/feelin-frisky/art/items_safe/" + rand_png)
+		set_texture(rand_texture)
+	#scale.x = 0.025 # Scale factor for Kenney prototype textures.
+	#scale.y = 0.025
+	scale.x = 1.125
+	scale.y = 1.125
