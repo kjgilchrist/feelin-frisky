77572c663250675114fa5bc02c2accb16c30d115
Katherine Gilchrist <93557317+kjgilchrist@users.noreply.github.com>
2023-05-02 11:46:11 -0600

    ui theme

--- feelin-frisky/src/arduino/Hall_Sensor_Simple/Hall_Sensor_Simple.ino
+++ feelin-frisky/src/arduino/Hall_Sensor_Simple/Hall_Sensor_Simple.ino
@@ -7,11 +7,26 @@ void setup() {
 
 void loop() {
   // Copy, pasta, change pin for more sensors.
+  int sensor0 = analogRead(A0);
   int sensor1 = analogRead(A1);
   int sensor2 = analogRead(A2);
   int sensor3 = analogRead(A3);
   int sensor4 = analogRead(A4);
+  int sensor5 = analogRead(A5);
+  int sensor6 = analogRead(A6);
+  int sensor7 = analogRead(A7);
+  int sensor8 = analogRead(A8);
+  int sensor9 = analogRead(A9);
+  int sensor10 = analogRead(A10);
+  int sensor11 = analogRead(A11);
+  int sensor12 = analogRead(A12);
+  int sensor13 = analogRead(A13);
+  int sensor14 = analogRead(A14);
+  int sensor15 = analogRead(A15);
+  int sensor16 = analogRead(A16);
   // Copy, pasta, change number for more sensors.
+  Serial.print(sensor0);
+  Serial.print(", ");
   Serial.print(sensor1);
   Serial.print(", ");
   Serial.print(sensor2);
@@ -19,6 +34,30 @@ void loop() {
   Serial.print(sensor3);
   Serial.print(", ");
   Serial.print(sensor4);
+  Serial.print(", ");
+  Serial.print(sensor5);
+  Serial.print(", ");
+  Serial.print(sensor6);
+  Serial.print(", ");
+  Serial.print(sensor7);
+  Serial.print(", ");
+  Serial.print(sensor8);
+  Serial.print(", ");
+  Serial.print(sensor9);
+  Serial.print(", ");
+  Serial.print(sensor10);
+  Serial.print(", ");
+  Serial.print(sensor11);
+  Serial.print(", ");
+  Serial.print(sensor12);
+  Serial.print(", ");
+  Serial.print(sensor13);
+  Serial.print(", ");
+  Serial.print(sensor14);
+  Serial.print(", ");
+  Serial.print(sensor15);
+  Serial.print(", ");
+  Serial.print(sensor16);
   // Final Line, so each print is different per frame.
   Serial.println();
 }
