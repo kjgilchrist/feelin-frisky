0dc6b0630192710dd34669d4fc8c6e0fc18469e2
Katherine Gilchrist <93557317+kjgilchrist@users.noreply.github.com>
2023-05-04 10:12:21 -0600

    game over and restart

--- feelin-frisky/src/feelin-frisky/scripts/game_over.gd
+++ feelin-frisky/src/feelin-frisky/scripts/game_over.gd
@@ -3,10 +3,22 @@ extends Control
 var is_paused: set = _set_is_paused, get = _get_is_paused
 @onready var game_background = get_tree().get_root().get_node("World/GUI/GameBackground")
 @onready var score_label = $CenterContainer/VBoxContainer/ScoreLabel
+@onready var quit_label = $QuitLabel
 @onready var fired_timer = $FiredTimer
 
 func _ready():
 	game_background.connect("game_over", _on_game_over)
+
+
+func _process(_delta):
+	if fired_timer.time_left > 0:
+		var fireTime = int(fired_timer.time_left)
+		if fireTime % 2 == 0:
+			quit_label.text = "Returning to Main Menu .."
+		elif fireTime % 3 == 0:
+			quit_label.text = "Returning to Main Menu ..."
+		else:
+			quit_label.text = "Returning to Main Menu ."
 
 
 func _set_is_paused(value):
